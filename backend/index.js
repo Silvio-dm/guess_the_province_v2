6a592019cf93ca677f0e099f9400668c2251f4a7
Silvio Di Monaco <silviodimonaco@gmail.com>
2024-05-02 14:50:22 +0200

    wakeup

--- backend/index.js
+++ backend/index.js
@@ -71,6 +71,10 @@ function checkLoggedIn(req, res) {
 */
 
 
+app.get('/api/wakeup', (req, res) => {
+  res.status(204).end();
+});
+
 app.get('/favicon.ico', (req, res) => {
   // Puoi rispondere con una risposta vuota o con un redirect, a seconda delle tue esigenze
   res.status(204).end(); // Rispondi con un codice di stato 204 (No Content)
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 //import GuestPlayButton from './guestButton.jsx'; // Importa il componente GuestPlayButton
@@ -13,6 +13,23 @@ import Game from './game.jsx'
 
 
 function App() {
+
+  const apiUrl = process.env.NODE_ENV === 'production' ? 'https://guess-the-province-v2-f9b590dd1051.herokuapp.com/' : 'http://localhost:3000';
+  useEffect(() => {
+    // Effettua una chiamata API al backend su Heroku all'avvio dell'applicazione
+    axios.get(`${apiUrl}/api/wakeup`)
+      .then(response => {
+        // Puoi gestire la risposta se necessario
+      })
+      .catch(error => {
+        console.error('Errore durante la chiamata API per risvegliare il server:', error);
+      });
+  }, []);
+
+
+
+
+
   const [gameData, setGameData] = useState({guestName: '', town_to_guess:'', message:'',provinces:[], record:null, score:0, logged: false}); // Stato per i dati del gioco
   const [page, setPage] = useState('home');
   
