be3ac9128ab51595a9457da45f7ffa1965378bb0
Silvio Di Monaco <silviodimonaco@gmail.com>
2024-05-07 16:55:03 +0200

    definitely maybe

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/assets/G9Lm1YYq.js', (req, res) => {
+app.get('/assets/index-G9Lm1YYq.js', (req, res) => {
   res.setHeader('Content-Type', 'application/javascript');
   res.sendFile(path.join(__dirname, '../dist/assets/index-G9Lm1YYq.js'));
 });
